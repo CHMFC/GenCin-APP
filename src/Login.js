7eb3d9794dae17ba0bd55e609b8f3c612905a5b2
CHMFC <chmfc@cin.ufpe.br>
2025-01-15 20:16:44 -0300

    Deploy da Sprint 3 do Homolog para a Main (#11)
    * Criando o Login e preparando o meio de campo para outras features
    
    * Adicionando a tela de Cadastro de Professor feita por Izack (#6)
    
    * Tela Aluno feita por Nicolas (#7)
    
    * Criando o Login e preparando o meio de campo para outras features (#5)
    
    * feature: Adicionado tela de cadastro do aluno
    
    * feature: Adicionado assets e reajustes nos blocos
    
    ---------
    
    Co-authored-by: NicBrito <nicolas_brito_barros@hotmail.com>
    
    * Tela de Login feita por Nicolas (#8)
    
    * feature: Adicionado tela de login
    
    * feature: Reajuste na formatação dos blocos
    
    ---------
    
    Co-authored-by: Nicolas Brito <nicolas_brito_barros@hotmail.com>
    
    * Tela Seletor Cadastro feita por Nicolas (#9)
    
    * Criando o Login e preparando o meio de campo para outras features (#5)
    
    * feature: Adicionado tela de seleção de cadastro
    
    * feature: Reajuste na formatação da logo
    
    ---------
    
    Co-authored-by: Nicolas Brito <nicolas_brito_barros@hotmail.com>
    
    * Tela Cadastro Professor e Aluno  (#10)
    
    * Adicionando a tela de Cadastro de Professor feita por Izack
    
    * Novas telas de login-cadastro-professor-aluno
    
    ---------
    
    Co-authored-by: izackangelo07 <god0704@hotmail.com>
    
    ---------
    
    Co-authored-by: NicBrito <nicolas_brito_barros@hotmail.com>
    Co-authored-by: izackangelo07 <god0704@hotmail.com>

--- src/Login.js
+++ src/Login.js
@@ -1,138 +1,204 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     View,
-    StyleSheet,
     Text,
+    Image,
+    StyleSheet,
+    TextInput,
+    TouchableOpacity,
 } from "react-native";
 
-export default function Login(){
-
-
+export default function LoginScreen() {
+    const [passwordVisible, setPasswordVisible] = useState(false);
     return (
         <View style={styles.container}>
-
-            <View style={styles.centraLogin}>
-
-                <View style={styles.viewLogo}>
-                    <Text style={styles.Logo}> 
-                        GenCin
-                    </Text>
-                </View>
-
-                <View style={styles.viewEmail}>
-                    <Text style={styles.textoEmail}>
-                        E-mail
-                    </Text>
-                    <View style={styles.inputEmail}>
-
-                    </View>
-                </View>
-
-                <View style={styles.viewSenha}>
-                    <Text style={styles.textoSenha}>
-                        Senha
-                    </Text>
-                    <View style={styles.inputSenha}>
-
-                    </View>
-                </View>
-
-                <View style={styles.viewEsqueceuSenha}>
-                    <Text style={styles.textoEsqueceuSenha}>
-                        Esqueceu sua senha?
-                    </Text>
-                </View>
-
-                <View>
-
-                </View>
-
+            {/* Logo */}
+            <View style = {styles.viewLogo}>
+                <Image
+                    source = {require('../assets/GenCIn_BigLogo.png')}
+                    style = {styles.logo}
+                />
             </View>
 
+            {/* Campo do Email */}
+            <View style = {styles.viewEmail}>
+                <Text style = {styles.textEmail}>
+                    Email
+                </Text>
+                <TextInput
+                    style = {styles.inputEmail}
+                    placeholder = "email@email.com"
+                    placeholderTextColor = "rgba(169, 169, 169, 255)"
+                    keyboardType = "email-address"
+                />
+            </View>
 
+            {/* Campo da Senha */}
+            <View style={styles.viewSenha}>
+                <Text style={styles.textSenha}>Senha</Text>
+                <View style={styles.viewOlhoSenha}>
+                    <TextInput
+                        style={styles.inputSenha}
+                        placeholder="senha"
+                        placeholderTextColor="rgba(169, 169, 169, 255)"
+                        secureTextEntry={!passwordVisible}
+                    />
+                    <TouchableOpacity
+                        style={styles.buttonOlhoSenha}
+                        onPress={() => setPasswordVisible(!passwordVisible)}
+                    >
+                        <Image
+                            source={
+                                passwordVisible
+                                    ? require("../assets/SenhaRevelada.png")
+                                    : require("../assets/SenhaEscondida.png")
+                            }
+                            style={styles.imageOlhoSenha}
+                        />
+                    </TouchableOpacity>
+                </View>
+            </View>
 
+            {/* Texto: "Esqueceu a senha?" */}
+            <TouchableOpacity style = {styles.viewEsqueciSenha}>
+                <Text style = {styles.textEsqueciSenha}>
+                    Esqueceu a senha?
+                </Text>
+            </TouchableOpacity>
+
+            {/* Botão para entrar/login */}
+            <TouchableOpacity style = {styles.viewBotaoEntrar}>
+                <Text style = {styles.textBotaoEntrar}>
+                    Entrar
+                </Text>
+            </TouchableOpacity>
+
+            {/* Texto: "Ainda não tem conta? Cadastre-se!" */}
+            <TouchableOpacity style = {styles.viewCadastreSe}>
+                <Text style = {styles.textCadastreSe}>
+                    Ainda não tem conta? Cadastre-se!
+                </Text>
+            </TouchableOpacity>
 
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        paddingHorizontal: 20,
+        paddingVertical: 40,
+        backgroundColor: "rgba(206, 17, 17, 255)",
+    },
 
+    viewLogo: {
+        position: "absolute",
+        top: 50,
+        width: "100%",
+        alignItems: "center",
+    },
+    logo: {
+        width: 256,
+        height: 128,
+        resizeMode: "contain",
+    },
 
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    paddingHorizontal: 20,
-    paddingVertical: 40,
-  },
-  centraLogin: {
-    width: "100%",
-    height: "100%",
-    backgroundColor: 'rgba(255,0,0,255)',
-    borderRadius: 30,
-    justifyContent: "center",
-    alignItems: "center",
-  },
+    viewEmail: {
+        width: 328,
+        marginBottom: 20,
+    },
+    textEmail: {
+        color: "rgba(255, 255, 255, 255)",
+        fontFamily: "Roboto",
+        fontSize: 22,
+        fontWeight: "500",
+        marginBottom: 5,
+    },
+    inputEmail: {
+        backgroundColor: "rgba(255, 255, 255, 255)",
+        color: "rgba(0, 0, 0, 255)",
+        height: 55,
+        fontSize: 20,
+        borderRadius: 8,
+        paddingHorizontal: 10,
+    },
 
-  viewLogo: {
-    height: "10%",
-    width: "100%",
-    backgroundColor: 'rgba(255,255,255,255)',
-    justifyContent: "center",
-    alignItems: "center",
-    marginBottom: 20,
-  }, 
-  Logo: {
-    fontSize: 48,
-    fontWeight: 800,
-  },
+    viewSenha: {
+        width: 328,
+        marginBottom: 20,
+    },
+    textSenha: {
+        color: "rgba(255, 255, 255, 255)",
+        fontFamily: "Roboto",
+        fontSize: 22,
+        fontWeight: "500",
+        marginBottom: 5,
+    },
+    inputSenha: {
+        backgroundColor: "rgba(255, 255, 255, 255)",
+        color: "rgba(0, 0, 0, 255)",
+        flex: 1,
+        height: 55,
+        fontSize: 20,
+    },
+    viewOlhoSenha: {
+        backgroundColor: "rgba(255, 255, 255, 255)",
+        flexDirection: "row",
+        borderRadius: 8,
+        paddingHorizontal: 10,
+    },
+    buttonOlhoSenha: {
+        width: 40,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    imageOlhoSenha: {
+        width: 24,
+        height: 24,
+        tintColor: "rgba(0, 0, 0, 255)",
+    },
 
-  viewEmail: {
-    height: "14%",
-    width: "100%",
-    backgroundColor: 'rgba(255,255,255,255)',
-    alignItems: "left",
-    padding: 10,
-  },
-  textoEmail: {
-    fontSize: 24,
-    marginBottom: 10,
-  }, 
-  inputEmail: {
-    height: "50%",
-    borderWidth: 1,
-    borderColor: 'rgba(0,0,0,255)',
-    borderRadius: 10,
-  },
+    viewEsqueciSenha: {
+        width: 328,
+        marginBottom: 30,
+    },
+    textEsqueciSenha: {
+        color: "rgba(255, 255, 255, 255)",
+        fontFamily: "Roboto",
+        fontSize: 16,
+        textAlign: "right",
+        textDecorationLine: "underline",
+    },
 
-  viewSenha: {
-    height: "14%",
-    width: "100%",
-    backgroundColor: 'rgba(255,255,255,255)',
-    alignItems: "start",
-    marginBottom: 20,
-    padding: 10,
-  },
-  textoSenha: {
-    fontSize: 24,
-    marginBottom: 10,
-  }, 
-  inputSenha: {
-    height: "50%",
-    borderWidth: 1,
-    borderColor: 'rgba(0,0,0,255)',
-    borderRadius: 10,
-  },
+    viewBotaoEntrar: {
+        backgroundColor: "rgba(0, 123, 255, 255)",
+        width: 328,
+        alignItems: "center",
+        borderRadius: 8,
+        paddingVertical: 15,
+        marginBottom: 20,
+    },
+    textBotaoEntrar: {
+        color: "rgba(255, 255, 255, 255)",
+        fontFamily: "Roboto",
+        fontSize: 18,
+        fontWeight: "700",
+    },
 
-  viewEsqueceuSenha: {
-    width: "100%",
-    height: "10%",
-    backgroundColor: 'rgba(255,255,255,255)',
-  },
-  textoEsqueceuSenha: {
-    textAlign: "right",
-    fontSize: 24,
-  }
-
+    viewCadastreSe:{
+        position: "absolute",
+        bottom: 20,
+        width: "100%",
+        alignItems: "center",
+    },
+    textCadastreSe: {
+        color: "rgba(255, 255, 255, 255)",
+        fontFamily: "Roboto",
+        textAlign: "center",
+        fontSize: 16,
+        textDecorationLine: "underline",
+    },
 });
--- src/SeletorCadastro.js
+++ src/SeletorCadastro.js
@@ -3,19 +3,144 @@ import {
     View,
     StyleSheet,
     Text,
+    Image,
+    TouchableOpacity
 } from "react-native";
 
-export default function SeletorCadastro(){
+export default function SeletorCadastro() {
 
-    return( 
-    <View>
+    return (
+        <View style={styles.container}>
 
-    </View>
-    )
+            {/* Logo */}
+            <View style={styles.viewLogo}>
+                <Image
+                    source={require('../assets/GenCIn_Logo.png')}
+                    style={styles.logo}
+                />
+            </View>
+
+            {/* Texto: "Bem-vindo(a) ao GenCin!" */}
+            <View style={styles.viewBemVindo}>
+                <Text style={styles.textBemVindo}>
+                    Bem-vindo(a) ao GenCin!
+                </Text>
+            </View>
+
+            {/* Texto: "Quem é você no CIn?" */}
+            <View style={styles.viewQuemSou}>
+                <Text style={styles.textQuemSou}>
+                    Quem é você no CIn?
+                </Text>
+            </View>
+
+            {/* Botão para "Eu sou aluno" */}
+            <TouchableOpacity style={styles.botao}>
+                <Image
+                    source={require('../assets/EuSouAluno.png')}
+                    style={styles.imagemBotao}
+                />
+                <Text style={styles.textBotao}>
+                    Eu sou aluno
+                </Text>
+            </TouchableOpacity>
+
+            {/* Botão para "Eu sou professor" */}
+            <TouchableOpacity style={styles.botao}>
+                <Image
+                    source={require('../assets/EuSouProfessor.png')}
+                    style={styles.imagemBotao}
+                />
+                <Text style={styles.textBotao}>
+                    Eu sou professor
+                </Text>
+            </TouchableOpacity>
+
+            {/*Texto: "Já possui uma conta?"*/}
+            <TouchableOpacity style={styles.viewJaTenhoConta}>
+                <Text style={styles.textJaTenhoConta}>
+                    Já possui uma conta?
+                </Text>
+            </TouchableOpacity>
+
+        </View>
+    );
 
 }
-    
+
 const styles = StyleSheet.create({
 
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        paddingHorizontal: 20,
+        paddingVertical: 40,
+        backgroundColor: "rgba(206, 17, 17, 255)",
+    },
 
+    viewLogo: {
+        position: "absolute",
+        top: 50,
+    },
+    logo: {
+        width: 128,
+        height: 128,
+        resizeMode: "contain",
+    },
+
+    viewBemVindo: {
+        marginBottom: 10,
+    },
+    textBemVindo: {
+        color: "rgba(255, 255, 255, 255)",
+        fontFamily: "Roboto",
+        textAlign: "center",
+        fontSize: 26,
+        fontWeight: "700",
+    },
+
+    viewQuemSou: {
+        marginBottom: 20,
+    },
+    textQuemSou: {
+        color: "rgba(255, 255, 255, 255)",
+        fontFamily: "Roboto",
+        textAlign: "center",
+        fontSize: 24,
+        fontWeight: "500",
+    },
+
+    botao: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "rgba(255, 255, 255, 255)",
+        borderRadius: 8,
+        padding: 15,
+        marginVertical: 10,
+        width: 328,
+    },
+    imagemBotao: {
+        width: 50,
+        height: 50,
+        marginRight: 10,
+    },
+    textBotao: {
+        color: "rgba(0, 0, 0, 255)",
+        fontFamily: "Roboto",
+        fontSize: 18,
+        fontWeight: "600",
+    },
+
+    viewJaTenhoConta: {
+        position: "absolute",
+        bottom: 20,
+    },
+    textJaTenhoConta: {
+        color: "rgba(255, 255, 255, 255)",
+        fontFamily: "Roboto",
+        fontSize: 16,
+        textDecorationLine: "underline",
+    },
 });
